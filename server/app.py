d2a35121a846918f4b9fbe70e26a8fb7204657e8
Nyakio-Eva <evalkariuki@gmail.com>
2024-04-09 22:04:00 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,69 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        message_dict = [message.to_dict() for message in messages ]
+         
+        return make_response(
+            jsonify(message_dict),
+            200)
+    
+    elif request.method == 'POST':
+        request_data = request.get_json()
+        new_message = Message(
+            body = request_data["body"],
+            username = request_data["username"]      
+        )
+       
+        db.session.add(new_message)
+        db.session.commit()
+
+        return jsonify(new_message.to_dict()), 201
+    
+
+
+@app.route('/messages/<int:id>', methods= ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This record doesnot exist in our database. Please try again."
+        }
+        return make_response(response_body, 404)
+    
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+
+            return make_response(message_dict, 200)
+        
+        elif request.method == 'PATCH':
+            request_data = request.get_json()
+            for attr in request_data:
+                setattr(message, attr, request_data[attr])
+
+            db.session.add(message)
+            db.session.commit()
+
+            return jsonify(message.to_dict()), 200  
+        
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Message deleted"
+            }
+
+            return make_response(response_body, 200)
+        
 
 if __name__ == '__main__':
     app.run(port=5555)
